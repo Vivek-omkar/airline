593f967393dfaaab3bffa818857836e68b90f898
Vivek-omkar <vivekshetty659@gmail.com>
2025-08-14 16:25:51 +0530

    Create conn.java

---
+++ conn.java
@@ -0,0 +1,21 @@
+package AirlineManagementSysytem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Conn {
+    Connection c;
+    Statement s;
+
+    public Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/airlinemanagementsystem", "root", "root");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
