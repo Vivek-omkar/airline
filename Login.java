7da133a5f6d926b720a3ecb292ae5b44e0cc5078
Vivek-omkar <vivekshetty659@gmail.com>
2025-08-14 16:23:01 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,11 @@
+package airlinemanagementsystem;
+import javax.swing.*;
+public class Login extends JFrame{
+    public Login(){
+        setSize(400,250);
+      setVisible(true);  
+    }
+    public static void main(String args[]){
+        new Login();
+    }
+}
